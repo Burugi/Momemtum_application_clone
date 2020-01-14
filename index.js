192ac5c1d003c600911c4445ee2788c777813c79
burugi <pinkbin4566@gmail.com>
2020-01-14 16:12:13 +0900

    Momentum_Clone version 0.0.6
    DOM의 설명
    자바스크립트는 html의 내용을 가져와서 바꿀 수 있다.

--- index.js
+++ index.js
@@ -1,25 +1,5 @@
-const nicoInfo={
-    name:"Nico",
-    age:33,
-    gender:"Male",
-    isHandsome:true,
-    favMovies:["Along the gods","Lotr","Oldboy"],
-}
-
-console.log(console)//console은 객체 log는 함수
-
-function sayHello(name,age){
-    //console.log("hello!"+ name+"you are"+age+"years old"); 옛날에 변수와 사용하는 방법
-    //하지만 ``이게 백틱이다.
-    return `Hello ${name} you are ${age} years old`
-}
-const greetNicolas=sayHello("nicoloas",33);
-console.log(greetNicolas);
-
-const calculaor={
-plus:function(a,b){
-    return a+b;
-}
-}
-const plus=calculaor.plus(5,5);
-console.log(plus)
+const title=document.getElementById("title");
+title.innerHTML="Hi From JS";//이렇게 변경할 수 있음
+console.log(title)//DOM=Document Object Module
+//위의 내용을 가져와서 우리가 크롬에서 바꿀수 있음
+//자바스크립트는 그걸 객체로 바꿔줄것이다.
