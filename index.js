9eb521ddb7727619e95bab1d12d6b710e3d24105
burugi <pinkbin4566@gmail.com>
2020-01-14 17:26:26 +0900

    Momentum_Clone version 0.0.9
    name 대신 찾는 list
    contain, toggle 사용

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 const title=document.querySelector("#title");
 
-const BASE_COLOR ="red";
-const OHTER_COLOR="#2980b9";
+const CLICKED_CLASS="clicked"
 
 function handleCilck(){
-   const currentColor = title.style.color;
-   if(currentColor===BASE_COLOR){
-       title.style.color=OHTER_COLOR;
-   }else{
-       title.style.color=BASE_COLOR;
-   }
+    const hasClass=title.classList.contains(CLICKED_CLASS);
+//contain은 클래스안에 클래스이름 속성을 가지고 있는지 확인 할 수 있다.
+//toggle이 아래의 내용을 해준다.
+/*
+if(!hasClass){
+        title.classList.add(CLICKED_CLASS);
+    }else{
+        title.classList.remove(CLICKED_CLASS);
+        //클래스네임이 아닌 클래스리스트를 쓰면 btn 클래스 이름이 남아잇는 상태로 변경된다.
+    }
+    console.log(currentClass)
+} 
+*/
+title.classList.toggle(CLICKED_CLASS);    
 }
-
-function init(){
-    title.style.color=BASE_COLOR;
+function init(){ 
     title.addEventListener("mouseenter",handleCilck)
 }
+
 init();
