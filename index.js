d243e9fca19210f6ecde837a315d59179fddfb56
burugi <pinkbin4566@gmail.com>
2020-01-14 16:20:55 +0900

    Momentum_Clone version 0.0.7
    DOM으로 할 수 있는 다양한 형태 확인

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const title=document.getElementById("title");
+const title=document.querySelector("#title");//쿼리셀렉터는 처음으로 찾은 내용을 보여줌 아이디로 찾겠음
+const title=document.querySelector(".title");//클래스로 찾겠음
 title.innerHTML="Hi From JS";//이렇게 변경할 수 있음
-console.log(title)//DOM=Document Object Module
-//위의 내용을 가져와서 우리가 크롬에서 바꿀수 있음
-//자바스크립트는 그걸 객체로 바꿔줄것이다.
+title.style.color="red";
+document.title="I own you note";
+console.dir(title)//내가 title로 할 수 있는 것들
+//Tag Name은 h1,h2같은 것을 의미한다.
