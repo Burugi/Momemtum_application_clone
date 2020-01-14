0689c25d7b1f22e0e8c718a9dbdb330d344a07cf
burugi <pinkbin4566@gmail.com>
2020-01-14 15:57:34 +0900

    Momentum_Clone version 0.0.5
    함수, 객체 혼합사용법
    반환값 적용

--- index.js
+++ index.js
@@ -8,7 +8,18 @@ const nicoInfo={
 
 console.log(console)//console은 객체 log는 함수
 
-function sayHello(name){
-    console.log("hello!", name);
+function sayHello(name,age){
+    //console.log("hello!"+ name+"you are"+age+"years old"); 옛날에 변수와 사용하는 방법
+    //하지만 ``이게 백틱이다.
+    return `Hello ${name} you are ${age} years old`
 }
-sayHello("nicoloas");
+const greetNicolas=sayHello("nicoloas",33);
+console.log(greetNicolas);
+
+const calculaor={
+plus:function(a,b){
+    return a+b;
+}
+}
+const plus=calculaor.plus(5,5);
+console.log(plus)
