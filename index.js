aa1e36ccd4d36889f3ca2d5ad7d34092e0aa55a9
burugi <pinkbin4566@gmail.com>
2020-01-14 16:41:41 +0900

    Momentum_Clone version 0.0.7
    이벤트의 설명과 적용

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const title=document.querySelector("#title");//쿼리셀렉터는 처음으로 찾은 내용을 보여줌 아이디로 찾겠음
-const title=document.querySelector(".title");//클래스로 찾겠음
-title.innerHTML="Hi From JS";//이렇게 변경할 수 있음
-title.style.color="red";
-document.title="I own you note";
-console.dir(title)//내가 title로 할 수 있는 것들
-//Tag Name은 h1,h2같은 것을 의미한다.
+
+function handleResize(event){
+    console.log(event)
+};
+function handleClick(event){
+    title.style.color="red";
+}//한번만 클릭을 의미한다.
+title.addEventListener("click",handleClick);
+//여기서 handleResize()를 하면 바로 함수를 호출하는게 되므로 함수이름만 적기
