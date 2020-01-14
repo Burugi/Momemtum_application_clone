13abfbb57ce709385c2a4824a767b671f1d981fe
burugi <pinkbin4566@gmail.com>
2020-01-14 20:12:27 +0900

    Momentum_Clone version 0.1.1
    시계 부분 적용 성공
    분,초가 한자리 수 일 경우 01,02,로 표현하게 만듦

---
+++ clock.js
@@ -0,0 +1,18 @@
+const clockContatiner=document.querySelector(".js-clock")
+    ,clockTitle=clockContatiner.querySelector("h1");
+function getTime(){
+    const date=new Date();
+    const minutes=date.getMinutes();
+    const hours=date.getHours(); //console에서 해보는 것을 추천
+    const seconds=date.getSeconds();
+    clockTitle.innerText=`${
+        hours<10?`0${hours}`:hours
+    }:${minutes<10?0`${minutes}`:minutes}:${
+        seconds<10?`0${seconds}`:seconds
+    }`;//작은 if ===삼항 연산자.
+}
+    function init(){
+        getTime();
+        setInterval(getTime,1000);//단위가 밀리세컨드이므로 1000이다.
+    }
+init();
