403aa5c838b809870db3181d41f368ee70195ec6
burugi <pinkbin4566@gmail.com>
2020-01-14 15:49:26 +0900

    Momentum_Clone version 0.0.4
    함수사용법

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
-let a=221; //let 으로 변수선언, const로 고정변수 선언
-let daysOfWeek=["monday","tue","wed"];// 배열을 묶을때 []으로 사용한다.
-let b=a-5;
-
-//Object 는 각 value에 이름을 줄 수 있다.
-//object는 {}을 사용한다. 객체
-let nicoInfo={
+const nicoInfo={
     name:"Nico",
     age:33,
     gender:"Male",
     isHandsome:true,
     favMovies:["Along the gods","Lotr","Oldboy"],
 }
-//const 객체 안에 있는 내용은 바꿀 수 있지만 
-console.log(nicoInfo.gender);
 
-//내용을 정리하는 법은 두가지 1.배열 2.객체
-//배열안에 객체를 넣을 수 있음
-//html의 console을 보려면 F12를 누르고 console을 보면되는 것임
+console.log(console)//console은 객체 log는 함수
+
+function sayHello(name){
+    console.log("hello!", name);
+}
+sayHello("nicoloas");
